9e65d4b0c5d6b7f3d448d07a8afe8294a9793ee0
andersonbosa <andersonbosa0@gmail.com>
2024-05-16 22:02:31 -0300

    feat: enable error tracking for application and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 // import "@/styles/globals.css"
 // import type { Metadata } from "next"
 
+import Script from 'next/script'
+
 // export const metadata: Metadata = {
 //   title: "Anderson Bosa | Software Engineer - Penetration Tester - Bachelor in Information Systems",
 //   description: "Anderson has been an active software engineer since 2019. Enthusiast at Leadership, Ethical Hacking and Red Teams Operations, Anderson seeks excellence in his career by focusing on interdisciplinary learning.",
@@ -14,5 +16,6 @@ interface RootLayoutProps {
 export default function RootLayout ({ children, }: Readonly<RootLayoutProps>) {
   return <>
     {children}
+    <Script src="/assets/js/clarity.js"></Script>
   </>
 }
