f867f2a25a8a1395717b4f6e56b8fe0cf8fac36a
andersonbosa <andersonbosa0@gmail.com>
2024-05-28 22:12:42 -0300

    refactor/v2.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
-import createNextIntlPlugin from 'next-intl/plugin'
-
-const withNextIntl = createNextIntlPlugin()
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* https://lucide.dev/guide/packages/lucide-react#one-generic-icon-component */
-  // transpilePackages: ['lucide-react']
+const nextConfig = {};
 
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-        port: '',
-        pathname: '/**',
-      }
-    ]
-  }
-}
-
-export default withNextIntl(nextConfig)
+export default nextConfig;
