9cd50b7912efb0e4d74b5d85d908239f14a8ee8d
andersonbosa <andersonbosa0@gmail.com>
2024-06-23 14:58:55 -0300

    style: remove scrollbars from app

--- src/modules/__shared__/theme/common.tsx
+++ src/modules/__shared__/theme/common.tsx
@@ -4,37 +4,7 @@ import { GlobalStyles } from '@mui/material'
 export const CommonStyles = (
   <GlobalStyles
     styles={{
-      // '::-webkit-scrollbar': {
-      //   width: '12px',
-      // },
-      // '::-webkit-scrollbar-track': {
-      //   background: 'var(--bg-light)',
-      //   borderRadius: '10px',
-      // },
-      // '::-webkit-scrollbar-thumb': {
-      //   backgroundColor: 'var(--color-primary)',
-      //   borderRadius: '10px',
-      //   border: '3px solid var(--bg-light)',
-      // },
-      // '::-webkit-scrollbar-thumb:hover': {
-      //   backgroundColor: 'var(--color-secondary)',
-      // },
-      // '*': {
-      //   scrollbarWidth: 'thin',
-      //   scrollbarColor: 'var(--color-primary) var(--bg-light)',
-      // },
-      // '@media (prefers-color-scheme: dark)': {
-      //   '::-webkit-scrollbar-track': {
-      //     background: 'var(--bg-dark)',
-      //   },
-      //   '::-webkit-scrollbar-thumb': {
-      //     backgroundColor: 'var(--color-primary)',
-      //     border: '3px solid var(--bg-dark)',
-      //   },
-      //   '*': {
-      //     scrollbarColor: 'var(--color-primary) var(--bg-dark)',
-      //   },
-      // },
+      // ...
     }}
   />
 )
