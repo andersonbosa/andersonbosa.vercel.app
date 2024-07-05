cbc75fd915407c21ab7126ad04e3f4b3a9641ec7
andersonbosa <andersonbosa0@gmail.com>
2024-07-04 23:17:42 -0300

    feat: add mock test

--- src/app/api/repositories/route.ts
+++ src/app/api/repositories/route.ts
@@ -3,16 +3,11 @@ import { client } from '@/modules/__shared__/api/github-client'
 export async function GET () {
   const repos: Repository[] = await client.getUserRepositoriesByUsername('andersonbosa')
 
-  const favoriteRepositories = [
-    'security-goat',
-    'wayback.go',
-    'moshell.sh',
-    'git_audit_go',
-  ]
+  const filterCondition = (repo: Repository) => {
+    return repo?.topics?.includes('show')
+  }
 
-  const filteredRepositories = repos.filter(
-    ({ name }) => favoriteRepositories?.includes(name)
-  )
+  const filteredRepositories = repos.filter(filterCondition)
 
   const sortedRepositories = filteredRepositories.sort(
     (a, b) => b.stargazers_count - a.stargazers_count,
