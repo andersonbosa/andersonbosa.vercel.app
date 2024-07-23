3278494aa668a4ecfc3d2ce7ba8a1bcd1e634743
andersonbosa <andersonbosa0@gmail.com>
2024-07-23 19:46:16 -0300

    feat: Enhance hero header with scroll down functionality
    Introduce a visually appealing scroll down button in the hero header section. This button improves user experience by providing a clear visual cue and smooth scrolling behavior to navigate to the next section of the page.

--- src/modules/__shared__/components/hero-header/hero-header.tsx
+++ src/modules/__shared__/components/hero-header/hero-header.tsx
@@ -2,26 +2,40 @@
 
 import styles from './hero-header.module.css'
 
-import { Box } from '@mui/material'
+import { Box, IconButton } from '@mui/material'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 export interface IHeroHeaderProps {
 }
 
+const NEXT_SECTION_ID = '#about'
+
 export function HeroHeader (props: IHeroHeaderProps) {
+  const handleScrollDown = (): void => {
+    const nextSection: HTMLElement | null = document.getElementById(NEXT_SECTION_ID)
+    if (!nextSection) {
+      throw new Error('Could not find the next section')
+    }
+    nextSection.scrollIntoView({ behavior: 'smooth' })
+  }
   return (
     <Box id="hero-header" className={styles.heroHeader} >
-      <div className={styles.wrapper}>
-        <div className={styles.subWrapper}>
+      <Box className={styles.wrapper}>
+        <Box className={styles.subWrapper}>
           <p className={styles.content1}> Hello, friend.</p>
-          <div className={styles.content2}>
+          <Box className={styles.content2}>
             <span className={styles.iam}> i&apos;m </span>
             <h1>Anderson Bosa.</h1>
-          </div>
-          <div className={styles.content3}> <h3>I build secure digital experiences!</h3></div>
+          </Box>
+          <Box className={styles.content3}> <h3>I build secure digital experiences!</h3></Box>
           <p className={styles.content4}> Passionate about technology and hacking, software engineer
             since 2019.</p>
-        </div>
-      </div>
+        </Box>
+        <Box marginBottom={6}></Box>
+        <IconButton className={styles.scrollDownButton} onClick={handleScrollDown}>
+          <KeyboardArrowDownIcon />
+        </IconButton>
+      </Box>
     </Box>
   )
 }
