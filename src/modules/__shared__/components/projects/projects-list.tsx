1fc6a1bb31104d42e896ecff77d0fc2fafab0e53
andersonbosa <andersonbosa0@gmail.com>
2024-09-16 21:49:59 -0300

    refactor: adjust layout responsiveness for projects-list component

--- src/modules/__shared__/components/projects/projects-list.tsx
+++ src/modules/__shared__/components/projects/projects-list.tsx
@@ -1,15 +1,19 @@
 'use client'
 
-import { Grid } from '@mui/material'
+import { Grid, useTheme } from '@mui/material'
+
 import { ProjectCard } from './project-card'
+import { useIsMobile } from '../../hooks/is-mobile.hook'
 
 interface ProjectListProps {
   repositories: Repository[]
 }
 
 export const ProjectList: React.FC<ProjectListProps> = ({ repositories }) => {
+  const theme = useTheme()
+  const isMobile = useIsMobile(theme)
   return (
-    <Grid container spacing={3}>
+    <Grid container spacing={3} sx={{ width: isMobile ? '100vw' : 'inherit' }}>
       {
         repositories.map(
           (repo) => (
