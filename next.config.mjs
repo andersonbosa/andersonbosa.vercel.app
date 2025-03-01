7e50ea4ba17b1028a83316a3f4c7c76ab5edc2d4
andersonbosa <andersonbosa0@gmail.com>
2025-03-01 13:37:33 -0300

    feat(env): add DEVTO_USERNAME to .env.example and update .gitignore to include .env

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'dev.to',
+        port: '',
+        pathname: '/api/**',
+      },
     ],
   }
 }
