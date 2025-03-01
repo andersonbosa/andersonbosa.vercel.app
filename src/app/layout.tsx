56c5a00892d5a57172b50c74d3134ac669453d75
andersonbosa <andersonbosa0@gmail.com>
2025-03-01 09:17:24 -0300

    feat: add metadata constants and update layout and hero header components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
-import './globals.css'
 import '@/modules/__shared__/theme/common.css'
+import './globals.css'
 
 import type { Metadata } from "next"
-import Head from 'next/head'
 import { Roboto } from "next/font/google"
+import Head from 'next/head'
+import { meta } from './constants'
 
 const fontImported = Roboto({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700', '900'] })
 
 export const metadata: Metadata = {
-  title: 'Anderson Bosa',
-  description: 'I build secure digital experiences! Passionate about technology and hacking, software engineer since 2019',
-  creator: 'Anderson Bosa < https://github.com/andersonbosa >'
+  title: meta.title,
+  description: meta.description,
+  creator: meta.creator,
 }
 
-export default function RootLayout (
+export default function RootLayout(
   { children, }: Readonly<{ children: React.ReactNode }>
 ) {
   return (
-    <html lang="en_US">
+    <html lang='en'>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className={fontImported.className}>
         {children}
       </body>
