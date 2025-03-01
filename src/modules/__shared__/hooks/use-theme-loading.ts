3c6208c16a924fa41f1bd1a67d02954bd1cf269d
andersonbosa <andersonbosa0@gmail.com>
2025-03-01 10:51:57 -0300

    feat(loading): enhance loading component with message and structure; add useThemeLoading hook for theme management

---
+++ src/modules/__shared__/hooks/use-theme-loading.ts
@@ -0,0 +1,51 @@
+import { Theme } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { ThemeConfig } from '../theme/config'
+
+const LAST_VISIT_KEY = 'lastVisitTimestamp'
+const FAKE_LOADING_DURATION = 123
+const VISIT_THRESHOLD = 60 * 60 * 1000
+
+export const useThemeLoading = () => {
+    // TODO FIX está sempre usando valor padrão, ignorando a lógica do installFakeLoading
+    const [useFakeLoading, setUseFakeLoading] = useState(false)
+    const [currentThemeMode, setCurrentThemeMode] = useState<Theme>(ThemeConfig.themes.default)
+
+    const handleThemeChange = () => {
+        setCurrentThemeMode(
+            currentThemeMode.palette.mode === 'light'
+                ? ThemeConfig.themes.dark
+                : ThemeConfig.themes.light
+        )
+    }
+
+    const installPreferredColorScheme = () => {
+        const handleColorSchemeChange = (event: MediaQueryListEvent) => {
+            setCurrentThemeMode(event.matches ? ThemeConfig.themes.dark : ThemeConfig.themes.light)
+        }
+
+        const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
+        handleColorSchemeChange({ matches: mediaQuery.matches } as MediaQueryListEvent)
+        mediaQuery.addEventListener('change', handleColorSchemeChange)
+        return () => mediaQuery.removeEventListener('change', handleColorSchemeChange)
+    }
+
+    const installFakeLoading = () => {
+        const lastVisitTimestamp = localStorage.getItem(LAST_VISIT_KEY)
+        const now = Date.now()
+
+        if (!lastVisitTimestamp || now - parseInt(lastVisitTimestamp, 10) > VISIT_THRESHOLD) {
+            const timer = setTimeout(() => setUseFakeLoading(false), FAKE_LOADING_DURATION)
+            return () => clearTimeout(timer)
+        } else {
+            setUseFakeLoading(false)
+        }
+
+        localStorage.setItem(LAST_VISIT_KEY, now.toString())
+    }
+
+    useEffect(installPreferredColorScheme, [])
+    useEffect(installFakeLoading, [])
+
+    return { currentThemeMode, useFakeLoading, handleThemeChange }
+}
